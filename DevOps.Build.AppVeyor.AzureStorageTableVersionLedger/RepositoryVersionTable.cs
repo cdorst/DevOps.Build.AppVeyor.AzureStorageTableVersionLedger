856ebe2326003bee835fb2df254f5fc9917b2572
Christopher Dorst <chris@dorst.email>
2018-03-24 15:46:08 -0500

    Add RepositoryVersionTable.cs

---
+++ DevOps.Build.AppVeyor.AzureStorageTableVersionLedger/RepositoryVersionTable.cs
@@ -0,0 +1,20 @@
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DevOps.Build.AppVeyor.AzureStorageTableVersionLedger
+{
+    /// <summary>Azure Table Storage entity representing a current repository version</summary>
+    public class RepositoryVersionTable : TableEntity
+    {
+        public RepositoryVersionTable()
+        {
+        }
+
+        public RepositoryVersionTable(string accountName, string repositoryName, string version)
+        {
+            PartitionKey = accountName; RowKey = repositoryName; Version = version;
+        }
+
+        /// <summary>Current version of repository</summary>
+        public string Version { get; set; }
+    }
+}
